923a38ec65d94c3b22a333699d98fa8d0ce01816
Mary Stuart Elder <mary.elder@duke.edu>
2020-05-19 17:55:44 -0400

    Add user switching logic

--- MMM-Gestures.js
+++ MMM-Gestures.js
@@ -21,88 +21,38 @@ Module.register('MMM-Gestures',{
 		// Swipe right to left --> update modules with Joe's info
 		if (notification === 'GESTURE0') {
 			this.pressed0 = true;
-			this.sendNotification("SHOW_ALERT", {type: "notification", title:"GES0(HR)", message:"brightness:" + payload%10});
-			this.sendNotification("BRIGHTNESS", payload, this.name);
+			this.sendNotification("SHOW_ALERT", {type: "notification", title:"Joe", message:"Joe's Profile"});
+			MM.getModules().withClass(['calendar', 'MMM-Traffic']).enumerate(function(module) {
+                module.hide(1000, function() {
+                    //Module hidden.
+                });
+            });
+			MM.getModules().withClass(['calendar2', 'MMM-Traffic2']).enumerate(function(module) {
+                module.show(1000, function() {
+                    //Module shown.
+                });
+            });
         }
 
 		// Swipe left to right --> update modules with Stu's info
-		if (notification === 'GESTURE1' && !this.pressed1) {
+		if (notification === 'GESTURE1') {
 			this.pressed1 = true;
-			this.sendNotification("SHOW_ALERT", {type: "notification", title:"GES1(HL)", message:"hide Wunderlist"}); 
-			MM.getModules().withClass('MMM-Wunderlist').enumerate(function(module) {
+			this.sendNotification("SHOW_ALERT", {type: "notification", title:"Stu", message:"Stu's Profile"}); 
+			MM.getModules().withClass(['calendar2', 'MMM-Traffic2']).enumerate(function(module) {
                 module.hide(1000, function() {
                     //Module hidden.
                 });
             });
-			           
-        }
-		else if (notification === 'GESTURE1' && this.pressed1) { 
-			this.pressed1 = false;
-			this.sendNotification("SHOW_ALERT", {type: "notification", title:"GES1(HL)", message:"show Wunderlist"});
-			MM.getModules().withClass('MMM-Wunderlist').enumerate(function(module) {
+			MM.getModules().withClass(['calendar', 'MMM-Traffic']).enumerate(function(module) {
                 module.show(1000, function() {
-                    //Module hidden.
+                    //Module shown.
                 });
             });
-   
-		}
-
-		// No gesture in awhile? Turn monitor off
-
-		// Gesture when screen was asleep? Turn monitor on
-		
-		if (notification === 'GESTURE2' && !this.pressed2) {
-			this.pressed2 = true;
-			this.sendNotification("SHOW_ALERT", {type: "notification", title:"GES2(RL)", message:"monitor off"}); 
-			
-        }
-		else if (notification === 'GESTURE2' && this.pressed2) { 
-			this.pressed2 = false;
-			this.sendNotification("SHOW_ALERT", {type: "notification", title:"GES2(RL)", message:"monitor on"});
-			
-   
-		}
-		if (notification === 'GESTURE3') {
-			this.sendNotification("SHOW_ALERT", {type: "notification", title:"GES3(LR)", message:"Video"}); 
-			this.sendNotification("VIDEO", true);
-			
-			
-            MM.getModules().exceptWithClass(['clock','currentweather']).enumerate(function(module) {
-                module.hide(1000, function() {
-                    //Module hidden.
-                });
-            });
-			
-            
-        }
-					
+        }			
 	},
-	notificationReceived: function(notification, payload, sender) {
-		//register MMM-Button click
-		if (notification == 'VIDEO_STOP') {
-				MM.getModules().exceptWithClass(['clock','currentweather']).enumerate(function(module) {
-                module.show(1000, function() {
-                    //Module hidden.
-                });
-            });
-			
-			
-		}
-		if (notification == "BRIGHTNESS") {
-			this.config.brightness = payload;
-			Log.info(this.name + " received " + notification + " and set brightness to " + this.config.brightness);
-			this.sendSocketNotification("BRIGHTNESS", payload);
-			//Log.log(this.name + " received a module notification: " + notification + " from sender: " + sender.name);
-		} 
-	},
-
-	
-
 	start: function() {
 		this.pressed0 = false;
 		this.pressed1 = false;
-		this.pressed2 = false;
-		this.pressed3 = false;
 		this.sendSocketNotification('GESTURE_CONFIG', this.config);
 		Log.info('Starting module: ' + this.name);
 	},
